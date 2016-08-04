0449f2cb544a876f288a60c0f296bee474fb8ecf
kampeepan <botaney_babe@hotmail.com>
2016-08-04 15:02:06 +0700

    Sound Effect

--- app/src/main/java/egat/kampeepan/showtraffic/MainActivity.java
+++ app/src/main/java/egat/kampeepan/showtraffic/MainActivity.java
@@ -1,13 +1,40 @@
 package egat.kampeepan.showtraffic;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    private ListView listView;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-}
+        setContentView(R.layout.activity_main); //Statement
+
+        //Bind Widget
+        listView = (ListView) findViewById(R.id.listView);  //R=ที่เก็บรายชื่อ widget ทั้งหมด
+        button = (Button) findViewById(R.id.button);    //Alt+enter = cast to ...
+
+        //Button Controller
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Sound Effect
+                MediaPlayer mediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.cat);
+                mediaPlayer.start();
+
+            }   //onClick
+        });
+
+    }   //Main Method
+
+
+}   //Main Class
